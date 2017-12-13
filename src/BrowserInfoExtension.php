44b8f6ef25d429574c733bfda40ca81c6ad2edaf
Lodewijk Evers <lodewijk@twokings.nl>
2017-12-13 17:39:33 +0100

    add /browser

--- src/BrowserInfoExtension.php
+++ src/BrowserInfoExtension.php
@@ -3,6 +3,11 @@
 namespace Bolt\Extension\Jadwigo\BrowserInfo;
 
 use Bolt\Extension\SimpleExtension;
+use Silex\Application;
+use Silex\ControllerCollection;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use UAParser\Parser;
 
 /**
  * BrowserInfo extension class.
@@ -12,9 +17,47 @@ use Bolt\Extension\SimpleExtension;
 class BrowserInfoExtension extends SimpleExtension
 {
 
-    protected $browser;
+    private $app;
+    private $browser;
+  
+    public function __construct()
+    {
+        $this->app = $this->getContainer();
+        // set variables that are needed later
 
+        $ua = $_SERVER['HTTP_USER_AGENT'];
 
+        $parser = Parser::create();
+        $this->browser = $parser->parse($ua);
+
+        //$this->browser = '';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerFrontendRoutes(ControllerCollection $collection)
+    {
+        // All requests to /browser
+        $collection->match('/browser', [$this, 'callbackBrowser']);
+    }
+    
+    /**
+     * @param Application $app
+     * @param Request     $request
+     *
+     * @return Response
+     */
+    public function callbackBrowser(Application $app, Request $request)
+    {
+        if(!$this->app) {
+            $this->app = $app;
+        }
+        //dump($this->app);
+        return $this->app['twig']->render('@BrowserInfo/info.twig', [
+            'browser' => $this->browser
+        ]);
+    }
 
     /**
      * {@inheritdoc}
@@ -33,7 +76,7 @@ class BrowserInfoExtension extends SimpleExtension
     protected function registerTwigPaths()
     {
         return [
-            'templates'
+            'templates' => ['namespace' => 'BrowserInfo']
         ];
     }
 
@@ -55,7 +98,7 @@ class BrowserInfoExtension extends SimpleExtension
      */
     public function browserInfo()
     {
-        return $this->renderTemplate('info.twig', [
+        return $this->renderTemplate('@BrowserInfo/info.twig', [
             'browser' => $this->browser
         ]);
     }
