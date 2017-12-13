c9b2dbdfc851b7cf76013d47aa18212ed759ba87
Lodewijk Evers <lodewijk@twokings.nl>
2017-12-13 17:22:02 +0100

    initial commit

---
+++ src/BrowserInfoExtension.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bolt\Extension\Jadwigo\BrowserInfo;
+
+use Bolt\Extension\SimpleExtension;
+
+/**
+ * BrowserInfo extension class.
+ *
+ * @author Lodewijk Evers <lodewijk@twokings.nl>
+ */
+class BrowserInfoExtension extends SimpleExtension
+{
+}
